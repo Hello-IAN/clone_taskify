1f500379d6c5d5afca7cd6f53c4d6fcddd8080c1
Hello-IAN <soonsak0722@gmail.com>
2024-01-04 21:54:50 +0900

    [Frontend]refactor:change useOrganizaionList to commonAdaptor

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-controller.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-controller.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { useParams } from "next/navigation";
-import { useOrganizationList } from "@clerk/nextjs";
 import { useEffect } from "react";
+import { useParams } from "next/navigation";
+
+import { useCommonUseOrganization } from "@/components/commonAdaptor/useOrganization";
 
 /* url로 접근했을때, 접근한 orgId로 switcher 업데이트 */ 
 export const OrgControlloer = () => {
   const params = useParams();
-  const { setActive } = useOrganizationList();
+  const { setActive } = useCommonUseOrganization().useOrganizationList();
 
   useEffect(()=> {
     if (!setActive) return ;
