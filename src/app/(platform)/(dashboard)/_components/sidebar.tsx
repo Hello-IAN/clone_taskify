e42201c02cd5a4c6d016b1dc9988b843a10d1c92
Hello-IAN <soonsak0722@gmail.com>
2024-02-04 13:09:44 +0900

    [frontend]feat: add select organizaion and accordion in sidebar

--- src/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+import Link from "next/link";
+import { Plus } from "lucide-react";
+
+
+import { Accordion } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { sideBarActions, SidebarProps } from "../_interface/ISideBar";
 
@@ -25,9 +31,35 @@ export const DashboardSidebar = ({
 	)
 
 	return (
-		<div>
-			SideBar테스트
-		</div>
+		<>
+			<div className="font-medium text-xs flex items-center mb-1">
+				<span className="pl-4">
+					Workspaces
+				</span>
+				<Button
+					asChild
+					type="button"
+					size="icon"
+					variant="ghost"
+					className="ml-auto"
+				>
+					<Link href="/select-org">
+						<Plus className="h-4 w-4" />
+					</Link>
+				</Button>
+			</div>
+			<Accordion
+				type="multiple"
+				defaultValue={defaultAccordionValue}
+				className="space-y-2"
+			>
+				{userMemberships.data?.map(({ organization }) => (
+					<p key={organization.id}>
+						{organization.id}
+					</p>
+				))}
+			</Accordion>
+		</>
 	)
 
 }
