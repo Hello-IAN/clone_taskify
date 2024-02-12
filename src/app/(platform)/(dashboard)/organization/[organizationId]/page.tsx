c5b346d5c8117bae83db86ca24e8a5bad955d589
Hello-IAN <soonsak0722@gmail.com>
2024-02-13 02:43:08 +0900

    [frontend]feat: add board page

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,11 @@
+import { OrganizationBoardFormView } from "./_components/boardFormView"
+import { OrganizationBoardItemView } from "./_components/boardItemView"
+
 const OrganizationIdPage = () => {
   return (
-    <div>
-      OrganiztionIdPage
+    <div className="flex flex-col space-y-4">
+      <OrganizationBoardFormView />
+      <OrganizationBoardItemView />
     </div>
   )
 }
