e44c689dfa3bb11df48288524d12949eb94bf1ee
Hello-IAN <soonsak0722@gmail.com>
2024-02-13 22:50:50 +0900

    [frontend]refactor: change import list

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
 
 import { IOrganizationBoardProps } from "../../../_interface/IDashboard";
+import { deleteBoard } from "@/actions/boardServerAction";
 
 export const Board = ({
 	title,
 	id
 }: IOrganizationBoardProps) => {
+
+	const deleteBoardWithId = deleteBoard.bind(null, id);
+
 	return (
-		<form className="flex items-center gap-x-2">
+		<form action={deleteBoardWithId} className="flex items-center gap-x-2">
 			<p>
 				{title}
 			</p>
