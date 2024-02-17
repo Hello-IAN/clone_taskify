9283b972e3218f6929380da4a1225df55e5c6c75
Hello-IAN <soonsak0722@gmail.com>
2024-02-18 01:23:58 +0900

    [frontend]feat: add common form input and error components and add shadcn-ui/label

--- src/Interface/ICommon.ts
+++ src/Interface/ICommon.ts
@@ -3,3 +3,21 @@ export interface ISpesificProps {
 	id:string,
 	suffix:string
 }
+
+export interface IFormInputProps {
+	id: string;
+	label?: string;
+	type?: string;
+	placeholder?: string;
+	required?: boolean;
+	disabled?: boolean;
+	errors?: Record<string, string[] | undefined>;
+	className?: string;
+	defaultValue?: string;
+	onBlur?: () => void;
+}
+
+export interface IFormErrorsProps {
+	id: string;
+	errors?: Record<string, string[] | undefined>;
+}
