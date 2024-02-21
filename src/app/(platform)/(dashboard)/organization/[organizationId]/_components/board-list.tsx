011cf0bcf979f82e60c49b0a157b6c809b059151
Hello-IAN <soonsak0722@gmail.com>
2024-02-21 20:35:13 +0900

    [frontend]feat: add Separator and board-list css

---
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,25 @@
+import { User2 } from "lucide-react"
+
+export const Boardlist = () => {
+	return (
+		<div className="space-y-4">
+			<div className="flex items-center font-semibold text-lg">
+				<User2 className="h-6 w-6 mr-2"/>
+				Your boards
+			</div>
+			<div className="grid grid-cols-2 sm:gird-cols-3 lg:grid-cols-4 gap-4">
+				<div
+					role="button"
+					className="aspect-video relative h-full w-full bg-mute rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+				>
+					<p className="text-sm">
+						Creater New Board
+					</p>
+					<span text-xs>
+						5개 남았습니다.
+					</span>
+				</div>
+			</div>
+		</div>
+	)
+}
--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,14 @@
+import { Separator } from "@/components/ui/separator"
 import { Info } from "./_components/info"
 
 const OrganizationIdPage = () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator />
+      <div className="px-2 md:px-4">
+        
+      </div>
     </div>
   )
 }
