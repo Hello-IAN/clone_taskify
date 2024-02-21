f346609827e204680fe6043baf765c47980b62e6
Hello-IAN <soonsak0722@gmail.com>
2024-02-21 20:55:23 +0900

    [frontend]fix: fixed miss spell

--- src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,4 +1,6 @@
-import { User2 } from "lucide-react"
+import { HelpCircle, User2 } from "lucide-react";
+
+import { Hint } from "@/components/hint";
 
 export const Boardlist = () => {
 	return (
@@ -7,17 +9,27 @@ export const Boardlist = () => {
 				<User2 className="h-6 w-6 mr-2"/>
 				Your boards
 			</div>
-			<div className="grid grid-cols-2 sm:gird-cols-3 lg:grid-cols-4 gap-4">
+			<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
 				<div
 					role="button"
-					className="aspect-video relative h-full w-full bg-mute rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+					className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
 				>
 					<p className="text-sm">
 						Creater New Board
 					</p>
-					<span text-xs>
+					<span className="text-xs">
 						5개 남았습니다.
 					</span>
+					<Hint
+						sideOffset={40}
+						description={
+							`무료 작업실은 5개의 보드까지 만들 수 있습니다. 무제한 사용을 원하시면 작업실의 등급을 올려주세요!`
+						}
+					>
+						<HelpCircle 
+							className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+						/>
+					</Hint>
 				</div>
 			</div>
 		</div>
--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,14 @@
 import { Separator } from "@/components/ui/separator"
 import { Info } from "./_components/info"
+import { Boardlist } from "./_components/board-list"
 
 const OrganizationIdPage = () => {
   return (
     <div className="w-full mb-20">
       <Info />
-      <Separator />
+      <Separator className="my-4"/>
       <div className="px-2 md:px-4">
-        
+        <Boardlist />
       </div>
     </div>
   )
