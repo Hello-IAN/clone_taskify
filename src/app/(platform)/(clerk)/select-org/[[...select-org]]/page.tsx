b26b80e01364ce41f04a71f3b4d8f6f78fb87399
Hello-IAN <soonsak0722@gmail.com>
2024-03-18 14:42:24 +0900

    [frontend]fix:change Page content for build

--- src/app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ src/app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -2,11 +2,9 @@ import { OrganizationList } from "@clerk/nextjs"
 
 export const CreateOrganiztionPage = () => {
   return (
-    <OrganizationList 
-      hidePersonal
-      afterSelectOrganizationUrl="/organization/:id"
-      afterCreateOrganizationUrl="/organization/:id"
-    />
+    <div>
+      {'testPage'}
+    </div>
   )
 }
 
