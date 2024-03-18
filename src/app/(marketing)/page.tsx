1e3664b4b43a429b5a98755d822a2808c363c52c
Hello-IAN <soonsak0722@gmail.com>
2024-03-18 14:37:20 +0900

    [frontend]feat:testUrl

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -9,6 +9,8 @@ console.log(BASE_URL, 'inProd');
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 
+const BASE_URL = process.env.BASE_URL
+console.log(BASE_URL, 'inRelease');
 const fira_mono = Fira_Mono({ weight:"500", subsets: ['latin'] })
 const MarketingPage = () => {
   return (
